0b005740aff825cdc7101b4b092c37249e8c32f9
Harshit Maurya <130442677+mrharsh1@users.noreply.github.com>
2024-08-06 04:29:45 +0000

    new

--- 01_basics/dataType_summary.js
+++ 01_basics/dataType_summary.js
@@ -19,3 +19,33 @@ const myFunction = function() {
     console.log("myobj");
     
 }
+// **************************
+
+// stack(primitive), heap(non-primitive)
+// when memory use stack memory ...toh mujhe uski copy milti hai jo bo var delcere hua hai  
+// when memory usse heap memory..... toh wha se ref milta hai original value ka jha se hum jo change krenge vo original value me change hoha
+
+
+
+let mySchoolName = "MLML INTER COLLEGE";
+
+let anotherName = mySchoolName;
+anotherName = "ks saket pg college";
+
+
+console.log(mySchoolName );
+
+console.log(anotherName);
+
+
+let user1 = {
+    name: "USer1",
+    email: "user@gmail.com",
+    UPI: "user@ybl"
+}
+let user2 = user1;
+
+user2.email = "user@microsoft.com"
+console.log(user1.email);
+console.log(user2.email);
+
